d6c96f764771eb8fd695bedc53cf173721e73c2f
hausenshi <dvorak518@gmail.com>
2019-05-03 17:04:41 -0400

    boundary condition

--- shocktube/boundary_conditions.h
+++ shocktube/boundary_conditions.h
@@ -12,7 +12,12 @@
 //     reflecting_boundary_conditions<nx,nghost>(conservative);
 template<int nx, int nghost>
 void reflecting_boundary_conditions(array<array<double,nx>,3>& inout){
-  // YOU NEED TO IMPLEMENT THIS
+ for(int i=0; i<nghost; i++){
+   for(int j=0; j<3; j++){
+    inout[0][i] = inout[0][2*nghost-1-i];
+    inout[0][nx-1-i] = inout[0][nx-2*nghost+i];
+   }; 
+ }; // YOU NEED TO IMPLEMENT THIS
 }
 
 #endif
